3afa96a967801e80e5bc00b4f7b03fb47b65d610
rogtang <rogtang@gmail.com>
2020-07-03 13:25:56 -0700

    inital commit to repo

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar () {
+    return (
+<Link to='/'>
+    <h1>Click Link</h1>
+</Link>
+    )
+}
+
+export default Navbar;
+
