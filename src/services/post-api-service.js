9b95371138c2dbf7fbf4c9361cfa6283eb0d26b3
rogtang <rogtang@gmail.com>
2020-07-18 13:04:52 -0700

    refactor to fix delete handler

--- src/services/post-api-service.js
+++ src/services/post-api-service.js
@@ -1,31 +1,26 @@
-import TokenService from "./token-service";
-import config from "../config";
+import TokenService from './token-service'
+import config from '../config'
 
 const PostApiService = {
   getPosts() {
     return fetch(`${config.API_ENDPOINT}/posts`, {
       headers: {
-        authorization: `bearer ${TokenService.getAuthToken()}`,
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
       },
-    }).then((res) =>
-      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
-    );
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
   },
 
-  postPost(
-    name,
-    url,
-    address,
-    usernotes,
-    price_rating,
-    size_rating,
-    location_rating
-  ) {
+  postPost(name, url, address, usernotes, price_rating, size_rating, location_rating) {
     return fetch(`${config.API_ENDPOINT}/posts`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "content-type": "application/json",
-        authorization: `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
       },
       body: JSON.stringify({
         name,
@@ -34,38 +29,38 @@ const PostApiService = {
         usernotes,
         price_rating,
         size_rating,
-        location_rating,
+        location_rating
       }),
-    }).then((res) =>
-      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
-    );
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
   },
 
   deletePost(post_id, cb) {
     fetch(`${config.API_ENDPOINT}/posts/${post_id}`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        "content-type": "application/json",
-        authorization: `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
       },
     })
-      .then((res) => {
+      .then(res => {
         if (!res.ok) {
-          console.log(res);
-          return res.json().then((error) => Promise.reject(error));
+          console.log(res) 
+          return res.json().then(error => Promise.reject(error))
         }
-        return res.json();
+        return res.json()
       })
-      .then((data) => {
-        cb(post_id);
+      .then(data => {
+        cb(post_id)
       })
-      .then(() =>
-      window.location.assign("/posts")
-    )
-      .catch((error) => {
-        console.error(error);
-      });
-  },
-};
+      .catch(error => {
+        console.error(error)
+      })
+  }
+}
 
-export default PostApiService;
+export default PostApiService
