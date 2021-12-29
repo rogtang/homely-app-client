be5d762a3b426a3cfef65bff3655bbe1f12b83e5
rogtang <rogtang@gmail.com>
2021-12-28 19:03:02 -0800

    update api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: 'https://homely-server.herokuapp.com/api',
+    //API_ENDPOINT: 'https://homely-server.herokuapp.com/api',
+    API_ENDPOINT: window.location.hostname === "localhost"? 'http://localhost:8000/api' :'https://homely-server.herokuapp.com/api',
     //API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'homely-client-auth-token',
   }
