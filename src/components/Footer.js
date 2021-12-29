4337e9fa5fdcaea5a0046be1ecd16a27a0926ebf
rogtang <rogtang@gmail.com>
2021-12-29 11:33:00 -0800

    update logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,56 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
-    return (
-        <div className='app-footer'>
-                    <a href="https://github.com/rogtang" target="_blank" rel="noopener noreferrer"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" alt="github logo" className="social" /></a>
-                    <a href="mailto:rogtang@gmail.com" target="_blank" rel="noopener noreferrer"><img src="https://image.flaticon.com/icons/svg/732/732200.svg" alt="email icon" className="social" /></a>
-                    <a href="https://www.linkedin.com/in/rogtang" target="_blank" rel="noopener noreferrer"><img src="https://image.flaticon.com/icons/svg/49/49068.svg" alt="linked-in logo" className="social" /></a>
-                    <div className='footer-credit'>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-            </div>
-    )
+  return (
+    <div className="app-footer">
+      <a
+        href="https://github.com/rogtang"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+          alt="github logo"
+          className="social"
+        />
+      </a>
+      <a
+        href="mailto:rogtang@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-email-advertising-kiranshastry-solid-kiranshastry-1.png"
+          alt="email icon"
+          className="social"
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/rogtang"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+          alt="linked-in logo"
+          className="social"
+        />
+      </a>
+      <div className="footer-credit">
+        Icons made by{" "}
+        <a
+          href="https://www.github.com/"
+          title="Github"
+        >
+          GitHub
+        </a>{" "}, {" "}
+        <a href="https://icons8.com/icon/P0tX6ToLZPNj/email">Email icon by Icons8</a>{" "}
+        and {" "}
+        <a href="https://www.linkedin.com/" title="linkedin">
+          LinkedIn
+        </a>
+      </div>
+    </div>
+  );
 }
